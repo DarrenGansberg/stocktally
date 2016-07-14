9a977a4b7299a7e55b0bfc2d531d354a0f368f1c
Darren Gansberg <darmath.mxo@hotmail.com>
2016-07-14 10:58:14 +1000

    Add Application class that represents the application.
    The main() function is modified to create an instance of the Application
    class that represents the application. Upon instantiation the Application
    instance is initialised, for any required configuration, by reading
    the config file. Once initialised, run() is called by main() to provide
    a main application loop. The shutdown() method is added in order to
    perform any cleanup that the application may need before the application ends.

--- stocktally.py
+++ stocktally.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
+import configparser
+
+class Application:
+	def __init__(self,cfg_file):
+		self._config = configParser()
+		self._config.read_file(open(cfg_file))
+
+	def run():
+		while(True):
+			pass
+
+	def shutdown():
+		pass
+			
 
 def main():
-	pass	
+	try:
+		app = Application("config.cfg")
+	except KeyboardInterrupt:
+		app.shutdown()
 
 if __name__=="__main__": main()
 
