839ff92b8d9df490c0eebbd0638f8259167529f9
Darren Gansberg <darmath.mxo@hotmail.com>
2016-07-17 10:16:11 +1000

    Moved the stocktally.py module into the modules directory.

--- modules/stocktally.py
+++ modules/stocktally.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
+import sys
 import configparser
+import os
 
 class Application:
 	def __init__(self,cfg_file):
 		self._config = configparser.ConfigParser()
 		self._config.read_file(open(cfg_file))
+
+		#change sys.path to include location of custom modules
+		module_path = os.getcwd()+"/modules"
+		driver_path = module_path +"/drivers"
+		sys.path.append(module_path)
+		sys.path.append(driver_path)
+		print(sys.path)
+		#import modules
+		import display
+		import console 
+		self._display_controller = display.DisplayController(console.Console())
+		self._display_controller.DisplayStartupMessage()
+
 
 	def Run(self):
 		while(True):
