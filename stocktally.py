6d6dba49c6efaa8ce11ce53e580d615508df8daa
Darren Gansberg <darmath.mxo@hotmail.com>
2016-08-04 13:26:02 +1000

    Display startup message and main menu.
    Using the new application framework the application displays
    the startup message and main menu of the application.

--- stocktally.py
+++ stocktally.py
@@ -4,20 +4,34 @@ import os
 
 sys.path.append(os.getcwd()+"/modules")
 
-from application import Stocktally as Application
-from console import Console as Console
+from application import Application as Application
+import console
+
+class Stocktally(Application):
+	def __init__(self):
+		self._version = 0.01
+		self._display = console.Console()
+
+	def DisplayStartupMessage(self):
+		self._display.PrintString("Stocktally v"+str(self._version)+"\n")
+
+	def DisplayMainMenu(self):
+		self._display.PrintString("Stocktally main menu: \n")
+		self._display.PrintString("1. Update stock counts \n")
+		self._display.PrintString("2. Exit program \n")
+		self._display.PrintString("Enter Selection: \n")
 
 
 def main():
 	try:
-		app = Application("config.cfg")
-		console = Console(app)
-		console.DisplayStartupMessage()
-		console.DisplayMainMenu()
-		app.Run()
-	except FileNotFoundError:
-		print("\nconfig.cfg not found\n")
-		print("Verify that config.cfg exists, before retrying\n")
+		app = Stocktally()
+		app.DisplayStartupMessage()
+		app.DisplayMainMenu()
+
+	#except FileNotFoundError:
+	#	print("\nconfig.cfg not found\n")
+	#	print("Verify that config.cfg exists, before retrying\n")
+
 	except KeyboardInterrupt:
 		app.Shutdown()
 
