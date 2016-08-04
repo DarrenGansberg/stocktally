4b8b6c28f5ef4da7cab5b401de0218d64aa117bb
Darren Gansberg <darmath.mxo@hotmail.com>
2016-08-04 15:36:08 +1000

    Add Shutdown message display, implement messaging and cleanup.
    Shutdown() and _DisplayShutdownMessage() methods added to
    display a shutdown message to user upon program completion.
    
    ProcessMessage() added to implement messaging.
    
    Keyboard driver integrated for use by program.
    
    Run() method added to implement program message loop.
    
    Cleanup including:
    - Rename DisplayStartupMessage() to _DisplayStartupMessage()
    and DisplayMainMenu() to _DisplayMainMenu() to indicate
    methods intended as internal (private) methods only.

--- stocktally.py
+++ stocktally.py
@@ -6,28 +6,49 @@ sys.path.append(os.getcwd()+"/modules")
 
 from application import Application as Application
 import console
+import keyboard
 
 class Stocktally(Application):
 	def __init__(self):
+		super(Stocktally,self).__init__()
 		self._version = 0.01
 		self._display = console.Console()
+		self._input = keyboard.Keyboard(self.PostMessage)
 
-	def DisplayStartupMessage(self):
+	def _DisplayStartupMessage(self):
 		self._display.PrintString("Stocktally v"+str(self._version)+"\n")
 
-	def DisplayMainMenu(self):
+	def _DisplayMainMenu(self):
 		self._display.PrintString("Stocktally main menu: \n")
 		self._display.PrintString("1. Update stock counts. \n")
 		self._display.PrintString("2. Exit program. \n")
-		self._display.PrintString("Enter Selection: \n")
+		self._display.PrintString("Enter Selection: ")
 
+	def _DisplayShutdownMessage(self):
+		self._display.PrintString("\nThank you for using Stocktally\n")
+
+	def ProcessMessage(self, msg):
+		self._display.PrintString(str(msg.GetId()))
+
+
+	def Run(self):
+		self._DisplayStartupMessage()
+		self._DisplayMainMenu()
+		self._input.Start()
+		return super(Stocktally,self).Run()
+
+	def Shutdown(self):
+		self._DisplayShutdownMessage()
 
 def main():
 	try:
 		app = Stocktally()
-		app.DisplayStartupMessage()
-		app.DisplayMainMenu()
 
+		if app.Run():
+			#could report error to user here
+			pass
+		else:
+			app.Shutdown()
 	#except FileNotFoundError:
 	#	print("\nconfig.cfg not found\n")
 	#	print("Verify that config.cfg exists, before retrying\n")
