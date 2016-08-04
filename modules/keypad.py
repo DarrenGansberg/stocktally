a0cb4b046929bd76ef50a989062a32395c688497
Darren Gansberg <darmath.mxo@hotmail.com>
2016-08-04 12:51:33 +1000

    Add keypad module.
    The keypad module provides a basic "driver" to read input from
    a 4x4 matrix keypad. Key presses are read and passed to a
    defined callback function.

---
+++ modules/keypad.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import threading
+import messaging
+
+class Keypad(object):
+	def __init__(self, callback):
+		if callback is None:
+			raise Exception("callback cannot be None")
+		self._callback = callback
+		self._thread = threading.Thread(None, self._Read)
+
+	def _Read(self):
+		pass
+
+	def Start(self):
+		self._thread.start()
+
+
